c3b5f7f89b49102bbb996589475f7d5abd9ab714
ZeLourenco <zemiguel_lourenco@hotmail.com>
2017-02-01 18:47:47 +0000

    kss

--- ResultadoCarros.php
+++ ResultadoCarros.php
@@ -70,6 +70,7 @@
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/custom.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -79,7 +80,26 @@
     <![endif]-->
   </head>
   <body>
-
+	<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Stand Rodas & Motores</a>
+			</div>
+			
+			<div class="navbar-collapse collapse">
+					<ul class="nav navbar-nav navbar-right">
+					<li class="#Lojas"><a href="Lojas.php">Lojas</a></li>                    
+					</ul>
+			</div><!--/.nav-collapse -->
+	   </div>
+	</div>
+ 
   <table class="table table-striped">
     <thead>
       <tr>
