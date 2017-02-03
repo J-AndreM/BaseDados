9aa9a71b8c6dbf80bdd30059d449e4acaf8efff2
J-AndreM <joao.andrem@outlook.com>
2017-02-03 00:52:10 +0000

    login

--- Login.php
+++ Login.php
@@ -1,3 +1,31 @@
+<?php 
+
+	$username = "2012145811";
+	$password = "2012145811";
+	$basedados = "bd_2012145811";
+	$servidor = "delta.deec.uc.pt";
+	if(isset($_POST['login-submit'])){
+			$ligacao=mysqli_connect($servidor,$username,$password,$basedados);
+		if (mysqli_connect_errno()){
+			echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		}
+
+		$NIF = $_POST['username'];
+		$PASS = $_POST['password'];
+		$SQL="SELECT * FROM Funcionario WHERE NIF = '$NIF' AND Pass = '$PASS'";
+
+		if ($resultado=mysqli_query($ligacao,$SQL)){
+			$num=mysqli_num_rows($resultado);
+			if($num==0)
+				$loginbool=FALSE;
+			else
+				$loginbool=TRUE;
+		mysqli_close($ligacao);
+		}
+	}
+
+?>
+
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
@@ -19,90 +47,53 @@
 	</head>
 
 	<body>
-	
-	<div class="navbar navbar-default navbar-fixed-top" role="navigation">
-      <div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#">Stand Rodas & Motores</a>
-			</div>
-			
-			<div class="navbar-collapse collapse">
-					<ul class="nav navbar-nav navbar-right">
-					<li class="#Lojas"><a href="Lojas.php">Lojas</a></li>                    
-					</ul>
-			</div><!--/.nav-collapse -->
-	   </div>
-	</div>
-	
-<div class="container">
-    	<div class="row">
+		<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+		<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Stand Rodas & Motores</a>
+				</div>
+				
+				<div class="navbar-collapse collapse">
+						<ul class="nav navbar-nav navbar-right">
+						<li class="#Lojas"><a href="Lojas.php">Lojas</a></li>                    
+						</ul>
+				</div><!--/.nav-collapse -->
+		</div>
+		</div>
+
+		<!-- Pagina de Login !-->
+
+		<div class="container">
+			<?php
+			if((isset($_POST['login-submit']) && isset($loginbool) && $loginbool==FALSE) || (!isset($_POST['login-submit']))){
+				if(isset($loginbool) && $loginbool==FALSE)	{?>
+			<div class="row">
+				<div class="alert alert-danger">
+					<strong>Login Failed!</strong> Try again!
+				</div>
+			<?php }?>
+
 			<div class="col-md-6 col-md-offset-3">
-				<div class="panel panel-login">
-					<div class="panel-heading">
-						<div class="row">
-							<div class="col-xs-6">
-								<a href="#" class="active" id="login-form-link">Login</a>
-							</div>
-							<div class="col-xs-6">
-								<a href="#" id="register-form-link">Register</a>
-							</div>
-						</div>
-						<hr>
-					</div>
 					<div class="panel-body">
 						<div class="row">
 							<div class="col-lg-12">
-								<form id="login-form" action="http://phpoll.com/login/process" method="post" role="form" style="display: block;">
+								<form id="login-form" action="http://alunos.deec.uc.pt/~a2012145811/Login.php" method="post" role="form" style="display: block;">
 									<div class="form-group">
 										<input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
 									</div>
 									<div class="form-group">
 										<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
-									</div>
-									<div class="form-group text-center">
-										<input type="checkbox" tabindex="3" class="" name="remember" id="remember">
-										<label for="remember"> Remember Me</label>
 									</div>
 									<div class="form-group">
 										<div class="row">
 											<div class="col-sm-6 col-sm-offset-3">
 												<input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Log In">
-											</div>
-										</div>
-									</div>
-									<div class="form-group">
-										<div class="row">
-											<div class="col-lg-12">
-												<div class="text-center">
-													<a href="http://phpoll.com/recover" tabindex="5" class="forgot-password">Forgot Password?</a>
-												</div>
-											</div>
-										</div>
-									</div>
-								</form>
-								<form id="register-form" action="http://phpoll.com/register/process" method="post" role="form" style="display: none;">
-									<div class="form-group">
-										<input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
-									</div>
-									<div class="form-group">
-										<input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email Address" value="">
-									</div>
-									<div class="form-group">
-										<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
-									</div>
-									<div class="form-group">
-										<input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password">
-									</div>
-									<div class="form-group">
-										<div class="row">
-											<div class="col-sm-6 col-sm-offset-3">
-												<input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Register Now">
 											</div>
 										</div>
 									</div>
@@ -112,7 +103,20 @@
 					</div>
 				</div>
 			</div>
+			<?php } ?>
 
+			<!-- Login Efectuado !-->
+
+			<?php
+				if(isset($_POST["username"]) && isset($loginbool) && $loginbool==TRUE){
+			?>	
+				<div class="row">
+					<div class="alert alert-success">
+						<strong>Login Succefull!</strong> YES!
+					</div>
+				</div>
+			<?php }?>
+		</div>
 
 	</body>
 </html>
