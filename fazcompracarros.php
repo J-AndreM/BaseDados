0d0dd3ee63b18047a6602de16d6e736ecca47dd1
J-AndreM <joao.andrem@outlook.com>
2017-02-04 22:59:40 +0000

    bananas

--- fazcompracarros.php
+++ fazcompracarros.php
@@ -5,9 +5,23 @@ $password = "2012145811";
 $basedados = "bd_2012145811";
 $servidor = "delta.deec.uc.pt";
 
-
+// dados do carro
 $matricula = $_POST["Matricula"];
+$Modelo = $_POST["Modelo"];
+$NumLugares = $_POST["NumLugares"];
+$PrimeiroRegisto = $_POST["PrimeiroRegisto"];
+$Cor = $_POST["Cor"];
+$NumPortas = $_POST["NumPortas"];
+$Tipo = $_POST["Tipo"];
+$Potencia = $_POST["Potencia"];
+$Combustivel = $_POST["Combustivel"];
+$Quilometros = $_POST["Quilometros"];
+$Cilindrada = $_POST["Cilindrada"];
+$ID_Loja = $_POST["ID_Loja"];
+$Marca = $_POST["Marca"];
 $preco = $_POST["Preco"];
+
+// dados do cliente
 $NIF_Client = $_POST["NIF"];
 
 
@@ -21,12 +35,17 @@ if ($resultado=mysqli_query($ligacao,$SQL))
     {
         $SQL = "insert into Cliente (NIF) values($NIF_Client)";
         $resultado2 = mysqli_query($ligacao,$SQL);
+        echo $resultado2;
     }
 }
 
+$SQL = "insert into Carro values($matricula,$Modelo,$NumLugares,$PrimeiroRegisto,$Cor,$NumPortas,$Tipo,$Potencia,$Combustivel,$Quilometros,$Cilindrada,$preco,false,$ID_Loja,$Marca)";
+$resultado2 = mysqli_query($ligacao,$SQL);
+echo $resultado2;
 
 
 $SQL = "insert into CompraCarro values($matricula,$NIF_Client,CURDATE(),$preco)";
 $resultado2 = mysqli_query($ligacao,$SQL);
 mysqli_close($ligacao);
+echo $resultado2;
 ?>
